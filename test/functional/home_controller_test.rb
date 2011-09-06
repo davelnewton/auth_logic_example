1979851e7f05d2cdda8bb1dabf28a92df8472aec
Dave Newton <davelnewton@gmail.com>
2011-09-05 20:39:14 -0400

    Updated user fixture login/nickname fields; fixed controller test stub to expect redirect when not logged in.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class HomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert_response 302 # Redirected if not logged in.
   end
 
 end
